adb5ec3f97cea4f797f16fc9e367d0fbaa142fbd
Md Saquib <mdsaquib5540@gmail.com>
2026-06-11 18:58:54 +0530

    ok

--- src/data/products.js
+++ src/data/products.js
@@ -35,7 +35,6 @@ export const products = [
     collection: "Basant Bahaar",
     aspectRatio: "portrait", // 3:4
     images: [
-      "https://res.cloudinary.com/dhufjjp9t/image/upload/v1780986681/5_z2qjyp.jpg",
       "https://res.cloudinary.com/dhufjjp9t/image/upload/v1780986681/7_uqczj7.jpg",
       "https://res.cloudinary.com/dhufjjp9t/image/upload/v1780986682/3_d5bvgz.jpg",
       "https://res.cloudinary.com/dhufjjp9t/image/upload/v1780986684/2_hkflkm.jpg",
